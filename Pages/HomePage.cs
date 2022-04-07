089fbcd299e4c63064ae82dd7e18603026920100
Padmininellipudi <100193995+Padmininellipudi@users.noreply.github.com>
2022-04-07 17:49:28 +1200

    Spec Flow Project Files from Local

---
+++ Pages/HomePage.cs
@@ -0,0 +1,36 @@
+﻿using SeleniumPOM.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumPOM.Pages
+{
+    internal class HomePage
+    {
+        public void GoToTMPage(IWebDriver driver)
+        {
+            // Go to TM page
+            IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationDropdown.Click();
+            Wait.WaitToBeClickable(driver, "xPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a", 3);
+
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click(); 
+        }
+
+        public void GoToEmployeePage(IWebDriver driver)
+        {
+            // Go to Employee Page
+            IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationDropdown.Click();
+            Wait.WaitToBeClickable(driver, "xPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[2]/a", 3);
+
+            IWebElement empOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[2]/a"));
+            empOption.Click();
+            Console.WriteLine("Employee is clicked");
+        }
+    }
+}
---
+++ SpecFlow/TMFeature.feature
@@ -0,0 +1,25 @@
+﻿Feature: TMFeature
+
+As a TurnUp portal admin
+I would like to Create, Edit and Delete Time and Material Records
+To Manage employee's time and material successfully
+
+
+Scenario: Create time and material record with valid data
+	Given Logged into Turnup portal successfully
+	And I navigate to time and material page
+	When I created Time and Material record
+	Then Time and material record created successfully
+
+Scenario Outline: edit time and material record with valid data
+	Given logged into turnup portal successfully
+	And i navigate to time and material page
+	When i update '<description>' '<code>' '<price>' on an existing time and material record
+	Then time and material should have updated '<description>' '<code>' '<price>'
+
+	Examples: 
+	| description | code     | price |
+	| parrot      | keyboard | 10    |
+	| pigeon      | mouse    | 20    |
+	| ostrich     | pen      | 30    |
+
